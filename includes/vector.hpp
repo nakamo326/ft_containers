32fc4e6d5be6f39df141cc2b27661735b14109b7
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-01-20 16:43:37 +0900

    wip

--- includes/vector.hpp
+++ includes/vector.hpp
@@ -2,10 +2,61 @@
 #define FT_CONTAINERS_INCLUDES_VECTOR_HPP
 
 namespace ft {
-template <typename _T, typename _alloc = std::allocator<_T> >
+template <class T, class Allocator = std::allocator<T> >
 struct vector {
-private:
 public:
+  typedef T                                     value_type;
+  typedef Allocator                             allocator_type;
+  typedef std::size_t                           size_type;
+  typedef std::ptrdiff_t                        difference_type;
+  typedef T*                                    pointer;
+  typedef const pointer                         const_pointer;
+  typedef value_type&                           reference;
+  typedef const value_type&                     const_reference;
+  // 実実装要確認
+  typedef pointer                               iterator;
+  typedef const_pointer                         const_iterator;
+  typedef std::reverse_iterator<iterator>       reverse_iterator;
+  typedef std::reverse_iterator<const_iterator> const_reverse_iterator;
+
+  vector(){};
+  vector(const allocator_type& alloc) : _alloc(alloc){};
+  // vector(std::size_t n = 0, Allocator a = Allocator()){};
+  // ~vector() {};
+  // vector(const vector &x) {};
+  // vector &         operator=(const vector &x) {};
+
+  iterator         begin() { return _first; }
+  iterator         end() { return _last; }
+  const_iterator   cbegin() const { return _first; }
+  const_iterator   cend() const { return _last; }
+  reverse_iterator rbegin() { return static_cast<reverse_iterator>(_first); }
+  reverse_iterator rend() { return static_cast<reverse_iterator>(_last); }
+  size_type        size() { return _last - _first; }
+  bool             empty() { return _first == _last; }
+  size_type        capacity() { return _reserve_last - _first; }
+  reference        operator[](size_type i) { return _first[i]; }
+  const_reference  operator[](size_type i) const { return _first[i]; }
+  reference        at(size_type i) {
+    if (i >= size())
+      throw std::out_of_range("index is out of range.");
+    return _first[i];
+  }
+  const_reference at(size_type i) const {
+    if (i >= size())
+      throw std::out_of_range("index is out of range.");
+    return _first[i];
+  }
+  reference       front() { return _first; }
+  const_reference front() const { return _first; }
+  reference       back() { return _last - 1; }
+  const_reference back() const { return _last - 1; }
+
+private:
+  pointer        _first;
+  pointer        _last;
+  pointer        _reserve_last;
+  allocator_type _alloc;
 };
 }  // namespace ft
 
--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,29 +1,9 @@
 #include "ft_container.hpp"
 
 int main() {
-  int array[] = {1, 2, 3, 4, 5};
-  NS::vector<int> v(array, array + 5);
-  std::cout << "v.size() = " << v.size() << std::endl;
-  for (size_t i = 0; i < v.size(); ++i) {
-    std::cout << v[i] << " ";
-  }
-  std::cout << std::endl;
+  // int             array[] = {1, 2, 3, 4, 5};
+  NS::vector<int> v;
 
-  // test erase
-  std::cout << "========== erase ===========" << std::endl;
-  NS::vector<int>::iterator it = v.begin();
-  std::cout << "v.erase(it)" << std::endl;
-  NS::vector<int>::iterator it2 = v.erase(it);
-  std::cout << "v.size() = " << v.size() << std::endl;
-  std::cout << "it2 = " << *it2 << std::endl;
-  std::cout << "v.erase(it2)" << std::endl;
-  NS::vector<int>::iterator it3 = v.erase(it2);
-  std::cout << "v.size() = " << v.size() << std::endl;
-  std::cout << "it3 = " << *it3 << std::endl;
-
-  for (size_t i = 0; i < v.size(); ++i) {
-    std::cout << v[i] << " ";
-  }
   std::cout << std::endl;
   return 0;
 }
