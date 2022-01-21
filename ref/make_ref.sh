814f6bb1c3fa0c33f1e6b82a1b8295f1146e25c7
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-01-21 11:43:18 +0900

    add iterator traits

--- ref/make_ref.sh
+++ ref/make_ref.sh
@@ -8,5 +8,6 @@ git clone --depth 1 https://github.com/gcc-mirror/gcc
 cp llvm-project/libcxx/include/vector llvm_vector.cpp
 cp llvm-project/libcxx/include/__iterator/iterator_traits.h  llvm_iterator_traits.h
 cp llvm-project/libcxx/include/__iterator/wrap_iter.h  llvm_wrap_iter.h
+cp llvm-project/libcxx/include/__iterator/iterator_traits.h llvm_iterator_traits.h
 
 cp gcc/libstdc++-v3/include/bits/stl_vector.h gcc_vector.h
