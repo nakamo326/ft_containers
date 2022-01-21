85e8e56a8d1e24cc2cf86a4e9f100463ca131786
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-01-21 20:42:59 +0900

    fix shell script

--- ref/make_ref.sh
+++ ref/make_ref.sh
@@ -6,8 +6,7 @@ git clone --depth 1 https://github.com/gcc-mirror/gcc
 
 #copy vector source from llve
 cp llvm-project/libcxx/include/vector llvm_vector.cpp
-cp llvm-project/libcxx/include/__iterator/iterator_traits.h  llvm_iterator_traits.h
-cp llvm-project/libcxx/include/__iterator/wrap_iter.h  llvm_wrap_iter.h
 cp llvm-project/libcxx/include/__iterator/iterator_traits.h llvm_iterator_traits.h
+cp llvm-project/libcxx/include/__iterator/wrap_iter.h  llvm_wrap_iter.h
 
 cp gcc/libstdc++-v3/include/bits/stl_vector.h gcc_vector.h
