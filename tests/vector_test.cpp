54d23a0d739c69a7415d3d429ce6f99fa5ce6c04
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-01-22 11:10:26 +0900

    add test

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -2,14 +2,24 @@
 
 #include <gtest/gtest.h>
 
-class VectorTest : public ::testing::Test {};
-
-TEST_F(VectorTest, DefaultConstructor) {
-  ft::vector<int>  f;
+TEST(VectorTest, DefaultConstructor) {
   std::vector<int> s;
+  ft::vector<int>  f;
   EXPECT_EQ(s.size(), f.size());
   EXPECT_EQ(s.max_size(), f.max_size());
   EXPECT_EQ(s.data(), f.data());
   EXPECT_EQ(s.capacity(), f.capacity());
   EXPECT_EQ(s.get_allocator(), f.get_allocator());
+}
+
+TEST(VectorTest, At) {
+  std::vector<int> s(10);
+  ft::vector<int>  f(10);
+
+  s[5] = 10;
+  f[5] = 10;
+  EXPECT_EQ(s.at(0), f.at(0));
+  EXPECT_EQ(s.at(5), f.at(5));
+  EXPECT_THROW(s.at(20), std::out_of_range);
+  EXPECT_THROW(f.at(20), std::out_of_range);
 }
