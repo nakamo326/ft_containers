aeab50a32d35933a2f657e42450907d74948a6e5
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-01-23 15:19:44 +0900

    add test

--- tests/vector_test.cpp
+++ tests/vector_test.cpp
@@ -39,42 +39,97 @@ TEST(VectorTest, BeginAndEnd) {
 }
 
 TEST(VectorTest, Iterator) {
-  ft::vector<int> f(10);
+  std::vector<int> s(10);
+  ft::vector<int>  f(10);
   for (size_t i = 0; i < 10; i++) {
+    s[i] = i;
     f[i] = i;
   }
-  ft::vector<int>::iterator       it = f.begin();
-  ft::vector<int>::iterator       it2(it);
-  ft::vector<int>::const_iterator cit = f.begin();
-  EXPECT_EQ(true, (it == it2));
-  EXPECT_EQ(true, (it == cit));
-  EXPECT_EQ(true, (cit == it));
-  EXPECT_EQ(false, (it != it2));
-  EXPECT_EQ(false, (it != cit));
-  EXPECT_EQ(true, (it <= it2));
-  EXPECT_EQ(true, (it <= cit));
-  EXPECT_EQ(true, (it >= it2));
-  EXPECT_EQ(true, (it >= cit));
-  EXPECT_EQ(false, (it < it2));
-  EXPECT_EQ(false, (it < cit));
-  EXPECT_EQ(false, (it > it2));
-  EXPECT_EQ(false, (it > cit));
+  ft::vector<int>::iterator  it = f.begin();
+  ft::vector<int>::iterator  it2(it);
+  ft::vector<int>::iterator  cit = f.begin();
+  std::vector<int>::iterator sit = s.begin();
+  std::vector<int>::iterator sit2(sit);
+  std::vector<int>::iterator scit = s.begin();
+  EXPECT_EQ((sit == sit2), (it == it2));
+  EXPECT_EQ((sit == scit), (it == cit));
+  EXPECT_EQ((scit == sit), (cit == it));
+  EXPECT_EQ((sit != sit2), (it != it2));
+  EXPECT_EQ((sit != scit), (it != cit));
+  EXPECT_EQ((sit <= sit2), (it <= it2));
+  EXPECT_EQ((sit <= scit), (it <= cit));
+  EXPECT_EQ((sit >= sit2), (it >= it2));
+  EXPECT_EQ((sit >= scit), (it >= cit));
+  EXPECT_EQ((sit < sit2), (it < it2));
+  EXPECT_EQ((sit < scit), (it < cit));
+  EXPECT_EQ((sit > sit2), (it > it2));
+  EXPECT_EQ((sit > scit), (it > cit));
   cit++;
   it2++;
-  EXPECT_EQ(false, (it == it2));
-  EXPECT_EQ(false, (it == cit));
-  EXPECT_EQ(true, (it != it2));
-  EXPECT_EQ(true, (it != cit));
-  EXPECT_EQ(true, (it <= it2));
-  EXPECT_EQ(true, (it <= cit));
-  EXPECT_EQ(false, (it >= it2));
-  EXPECT_EQ(false, (it >= cit));
-  EXPECT_EQ(true, (it < it2));
-  EXPECT_EQ(true, (it < cit));
-  EXPECT_EQ(false, (it > it2));
-  EXPECT_EQ(false, (it > cit));
+  scit++;
+  sit2++;
+  EXPECT_EQ((sit == sit2), (it == it2));
+  EXPECT_EQ((sit == scit), (it == cit));
+  EXPECT_EQ((sit != sit2), (it != it2));
+  EXPECT_EQ((sit != scit), (it != cit));
+  EXPECT_EQ((sit <= sit2), (it <= it2));
+  EXPECT_EQ((sit <= scit), (it <= cit));
+  EXPECT_EQ((sit >= sit2), (it >= it2));
+  EXPECT_EQ((sit >= scit), (it >= cit));
+  EXPECT_EQ((sit < sit2), (it < it2));
+  EXPECT_EQ((sit < scit), (it < cit));
+  EXPECT_EQ((sit > sit2), (it > it2));
+  EXPECT_EQ((sit > scit), (it > cit));
   cit += 9;
   it2 += 9;
   EXPECT_EQ(true, (it2 == f.end()));
   EXPECT_EQ(true, (cit == f.end()));
+}
+
+TEST(VectorTest, ReverseIterator) {
+  std::vector<int> s(10);
+  ft::vector<int>  f(10);
+  for (size_t i = 0; i < 10; i++) {
+    s[i] = i;
+    f[i] = i;
+  }
+  ft::vector<int>::reverse_iterator        it = f.rbegin();
+  ft::vector<int>::reverse_iterator        it2(it);
+  ft::vector<int>::const_reverse_iterator  cit = f.rbegin();
+  std::vector<int>::reverse_iterator       sit = s.rbegin();
+  std::vector<int>::reverse_iterator       sit2(sit);
+  std::vector<int>::const_reverse_iterator scit = s.rbegin();
+  EXPECT_EQ((sit == sit2), (it == it2));
+  EXPECT_EQ((sit == scit), (it == cit));
+  EXPECT_EQ((scit == sit), (cit == it));
+  EXPECT_EQ((sit != sit2), (it != it2));
+  EXPECT_EQ((sit != scit), (it != cit));
+  EXPECT_EQ((sit <= sit2), (it <= it2));
+  EXPECT_EQ((sit <= scit), (it <= cit));
+  EXPECT_EQ((sit >= sit2), (it >= it2));
+  EXPECT_EQ((sit >= scit), (it >= cit));
+  EXPECT_EQ((sit < sit2), (it < it2));
+  EXPECT_EQ((sit < scit), (it < cit));
+  EXPECT_EQ((sit > sit2), (it > it2));
+  EXPECT_EQ((sit > scit), (it > cit));
+  cit++;
+  it2++;
+  scit++;
+  sit2++;
+  EXPECT_EQ((sit == sit2), (it == it2));
+  EXPECT_EQ((sit == scit), (it == cit));
+  EXPECT_EQ((sit != sit2), (it != it2));
+  EXPECT_EQ((sit != scit), (it != cit));
+  EXPECT_EQ((sit <= sit2), (it <= it2));
+  EXPECT_EQ((sit <= scit), (it <= cit));
+  EXPECT_EQ((sit >= sit2), (it >= it2));
+  EXPECT_EQ((sit >= scit), (it >= cit));
+  EXPECT_EQ((sit < sit2), (it < it2));
+  EXPECT_EQ((sit < scit), (it < cit));
+  EXPECT_EQ((sit > sit2), (it > it2));
+  EXPECT_EQ((sit > scit), (it > cit));
+  cit += 9;
+  it2 += 9;
+  EXPECT_EQ(true, (it2 == f.rend()));
+  EXPECT_EQ(true, (cit == f.rend()));
 }
