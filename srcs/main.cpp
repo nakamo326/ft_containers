e65ac95b01be8af3cab54a2f23700a3a2aee06d4
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-02-10 14:59:23 +0900

    wip

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,19 +1,9 @@
-#include "ft_container.hpp"
-#include "test_frame.hpp"
-#include "tests/vector_test.hpp"
+#include <cstddef>
+#include <vector>
 
 int main() {
-  {
-    Suite s("vector Suite");
-    s.add_test(Test("Vector_constructor", Vector_constructor));
-    s.add_test(
-        Test("vector_constructorWithAlloc", Vector_constructorWithAlloc));
-    s.add_test(Test("Vector_ConstructorWithSize", Vector_ConstructorWithSize));
-    s.add_test(
-        Test("Vector_ConstructorWithValue", Vector_ConstructorWithValue));
-    s.add_test(Test("Vector_RangeConstructor", Vector_RangeConstructor));
-    s.add_test(Test("Vector_CopyConstructor", Vector_CopyConstructor));
-    s.add_test(Test("Vector_AssignationOverload", Vector_AssignationOverload));
+  std::vector<int> v;
+  for (size_t i = 0; i < 1000; i++) {
+    v.push_back(i);
   }
-  return 0;
 }
