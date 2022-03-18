e419ef9535cf506518ea8c290cc2b62721c759fe
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-03-18 15:12:07 +0900

    add some bench

--- srcs/benchmark/vector_bench.cpp
+++ srcs/benchmark/vector_bench.cpp
@@ -61,14 +61,14 @@ void vec_range_assign_ft(void) {
 void vec_get_allocator_std(void) {
   std::vector<int> v;
 
-  for (size_t i = 0; i < 100000; i++) {
+  for (size_t i = 0; i < 1000000; i++) {
     v.get_allocator();
   }
 }
 void vec_get_allocator_ft(void) {
   ft::vector<int> v;
 
-  for (size_t i = 0; i < 100000; i++) {
+  for (size_t i = 0; i < 1000000; i++) {
     v.get_allocator();
   }
 }
@@ -92,6 +92,183 @@ void vec_array_op_ft(void) {
   }
 }
 
+void vec_at_std(void) {
+  int n;
+
+  for (size_t j = 0; j < 10; j++) {
+    for (size_t i = 0; i < 100000; i++) {
+      n = pre_v.at(i);
+    }
+  }
+}
+void vec_at_ft(void) {
+  int n;
+
+  for (size_t j = 0; j < 10; j++) {
+    for (size_t i = 0; i < 100000; i++) {
+      n = pre_f.at(i);
+    }
+  }
+}
+
+void vec_front_std(void) {
+  int n;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    n = pre_v.front();
+  }
+}
+void vec_front_ft(void) {
+  int n;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    n = pre_f.front();
+  }
+}
+
+void vec_back_std(void) {
+  int n;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    n = pre_v.back();
+  }
+}
+void vec_back_ft(void) {
+  int n;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    n = pre_f.back();
+  }
+}
+
+void vec_begin_std(void) {
+  std::vector<int>::iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_v.begin();
+  }
+}
+void vec_begin_ft(void) {
+  ft::vector<int>::iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_f.begin();
+  }
+}
+
+void vec_end_std(void) {
+  std::vector<int>::iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_v.end();
+  }
+}
+void vec_end_ft(void) {
+  ft::vector<int>::iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_f.end();
+  }
+}
+
+void vec_rbegin_std(void) {
+  std::vector<int>::reverse_iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_v.rbegin();
+  }
+}
+void vec_rbegin_ft(void) {
+  ft::vector<int>::reverse_iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_f.rbegin();
+  }
+}
+
+void vec_rend_std(void) {
+  std::vector<int>::reverse_iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_v.rend();
+  }
+}
+void vec_rend_ft(void) {
+  ft::vector<int>::reverse_iterator it;
+
+  for (size_t i = 0; i < 10000000; i++) {
+    it = pre_f.rend();
+  }
+}
+
+void vec_empty_std(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_v.empty()) {
+      num++;
+    };
+  }
+}
+void vec_empty_ft(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_f.empty()) {
+      num++;
+    };
+  }
+}
+
+void vec_size_std(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_v.size()) {
+      num++;
+    };
+  }
+}
+void vec_size_ft(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_f.size()) {
+      num++;
+    };
+  }
+}
+
+void vec_capacity_std(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_v.capacity()) {
+      num++;
+    };
+  }
+}
+void vec_capacity_ft(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_f.capacity()) {
+      num++;
+    };
+  }
+}
+
+void vec_max_size_std(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_v.max_size()) {
+      num++;
+    };
+  }
+}
+void vec_max_size_ft(void) {
+  int num = 0;
+  for (size_t i = 0; i < 10000000; i++) {
+    if (pre_f.max_size()) {
+      num++;
+    };
+  }
+}
+
 // ======================================================
 void vector_bench_entry(void) {
   for (size_t i = 0; i < 100000; i++) {
@@ -105,4 +282,15 @@ void vector_bench_entry(void) {
   Benchmark("rangeAssign", vec_range_assign_std, vec_range_assign_ft);
   Benchmark("getAllocator", vec_get_allocator_std, vec_get_allocator_ft);
   Benchmark("arrayOperator", vec_array_op_std, vec_array_op_ft);
+  Benchmark("at", vec_at_std, vec_at_ft);
+  Benchmark("front", vec_front_std, vec_front_ft);
+  Benchmark("back", vec_back_std, vec_back_ft);
+  Benchmark("begin", vec_begin_std, vec_begin_ft);
+  Benchmark("end", vec_end_std, vec_end_ft);
+  Benchmark("rbegin", vec_rbegin_std, vec_rbegin_ft);
+  Benchmark("rend", vec_rend_std, vec_rend_ft);
+  Benchmark("empty", vec_empty_std, vec_empty_ft);
+  Benchmark("size", vec_size_std, vec_size_ft);
+  Benchmark("capacity", vec_capacity_std, vec_capacity_ft);
+  Benchmark("max_size", vec_max_size_std, vec_max_size_ft);
 }
