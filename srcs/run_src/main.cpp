3dbb95324451004a082139e134c50e77c5170d7f
Yasufumi Nakamoto <nakamo326@gmail.com>
2022-03-29 11:26:20 +0900

    fix main

--- srcs/run_src/main.cpp
+++ srcs/run_src/main.cpp
@@ -45,49 +45,5 @@ int main() {
   //   print_map(v);
   // }
 
-  std::set<int> s;
-
-  s.insert(1);
-  s.insert(2);
-
-  std::set<int>::iterator it = s.begin();
-  *it                        = 3;
-
-  // {
-  //   ft::map<int, int> m;
-  //   for (size_t i = 0; i < 100000; i++) {
-  //     m.insert(ft::make_pair(i, i));
-  //   }
-  //   for (size_t i = 0; i < 20000; i++) {
-  //     m.erase(m.find(i), m.find(i + 5));
-  //   }
-  // }
-
   return 0;
 }
-
-// int main() {
-//   std::stringstream ss;
-//   ss << 1 << std::endl << 2 << std::endl << 3;
-
-//   std::istream_iterator<int> beg(ss);
-//   std::istream_iterator<int> last;
-
-//   ft::vector<int> v;
-//   v.assign(beg, last);
-//   for (ft::vector<int>::iterator it = v.begin(); it != v.end(); it++) {
-//     std::cout << *it << std::endl;
-//   }
-//   std::cout << "===========" << std::endl;
-//   std::vector<int> tmp(5);
-//   for (size_t i = 0; i < 5; i++) {
-//     tmp[i] = i;
-//   }
-
-//   v.assign(tmp.begin(), tmp.end());
-//   for (ft::vector<int>::iterator it = v.begin(); it != v.end(); it++) {
-//     std::cout << *it << std::endl;
-//   }
-
-//   return 0;
-// }
